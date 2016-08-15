835f75df53dac63a952c9bdd1d14e7f64d16d786
apeloza <apeloza@alumni.uwo.ca>
2016-08-15 14:10:39 -0500

    Simple CR functionality

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,11 +1,20 @@
 class ChaptersController < ApplicationController
+  before_action :set_chapter, only: [:show, :edit, :update, :destroy]
   def index
+    @chapters = Chapter.all
   end
 
   def new
+    @chapter = Chapter.new
   end
 
   def create
+    @chapter = Chapter.new(chapter_params)
+    if @chapter.save
+      redirect_to @chapter, notice: 'Chapter was successfully created'
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,9 +24,27 @@ class ChaptersController < ApplicationController
   end
 
   def update
+    if @chapter.update(chapter_params)
+      redirect_to @chapter, notice: 'Chapter was successfully updated'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @chapter.destroy
+    redirect_to chapters_path, notice: 'Chapter was successfully destroyed'
   end
+
+  private
+
+  def chapter_params
+    params.require(:chapter).permit(:title, :body, :complete)
+  end
+
+  def set_chapter
+    @chapter = Chapter.find(params[:id])
+  end
+
 
 end
